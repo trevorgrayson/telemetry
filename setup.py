8c2df3e318deb784caf1766ac699aa3f8be4f469
trevor grayson <trevor@dave.com>
2021-08-25 22:48:37 -0700

    hardcoding setup.py strings

--- setup.py
+++ setup.py
@@ -14,13 +14,52 @@ EXTRAS = { # 'statsd': ['statsd']
 
 setup(
     name='telemetry',
-    version=open("VERSION", "r").read(),
+    version='1.0.8',  # open("VERSION", "r").read(),
     packages=setuptools.find_packages(),
     # ['telemetry',],
     package_data={'': ['README.md', 'VERSION']},
     #package_dir={ 'telemetry': 'src/telemetry' },
     description="Remote measurements for your app",
-    long_description=open("README.md", "r").read(),
+    long_description="""
+Remote measuring abstraction for software applications.
+
+`telemetry` serves as a simple facade or abstraction for various telemetry frameworks (e.g. pagerduty, slack, graphite) 
+allowing the end user to plug in the desired telemetry framework at deployment time. Think [slf4j](http://www.slf4j.org/)
+but for events and numbers.  This library borrows from their example (and copy.)
+
+As your projects grow, their telemetry requirements will change.  The purpose of this library is to simplify
+implementation, provide easy configuration, encourage testing, and avoid vendor lock.
+
+## Supported Services:
+
+* pagerduty
+* slack
+
+Telemeters preference to being configurable, but don't require more than
+credentials to get working. For instance, slack can be implemented with
+the following:
+
+```SLACK_ROOM_ID=Txxx/Byyy/Zzzz python
+import logging
+from telemetry import SlackTelemeter
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger().addHandler(SlackTelemeter())
+logging.info("hello room!")
+```
+
+or 
+
+```SLACK_ROOM_ID=Txxx/Byyy/Zzzz python
+
+from telemetry import SlackTelemeter
+meter = SlackTelemeter()
+meter.message("your message!")
+```
+
+Clients are written using core python libraries, so `telemetry` is light weight.
+ 
+    """, # open("README.md", "r").read(),
     # long_description=open('README2.0.md').read(),
     long_description_content_type="text/markdown",
     author='trevor grayson',

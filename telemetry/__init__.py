5e3e2292ada84f90f4e10f44a91774d1ce26d3ed
Trevor Grayson <trevor@trevorgrayson.com>
2021-09-18 21:30:16 -0700

    re-refactor __init__

--- telemetry/__init__.py
+++ telemetry/__init__.py
@@ -7,35 +7,10 @@ from .telemeter import Telemeter
 # backwards compat
 from .wrappers import runtime
 from .deprecated import *
-from .load_utils import env_handlers
+from .load_utils import *
 
 REGISTRY = [
     PagerDutyTelemeter,
     SlackTelemeter
 ]
 ACTIVE_TELEMETERS = []
-
-
-def is_configured(meter: Telemeter):
-    return all([req in environ for req in meter.__requires__])
-
-
-def is_logger(meter: Telemeter):
-    return hasattr(loggers, meter.__name__)
-
-
-def env_loggers():
-    for meter in REGISTRY:
-        if is_configured(meter) and is_logger(meter):
-            yield meter
-
-
-def load_env(logger=None):
-    global ACTIVE_TELEMETERS
-    ACTIVE_TELEMETERS = []  # TODO scan if each telemeter is loaded already.
-    for meter in REGISTRY:
-        if is_configured(meter):
-            m = meter()
-            ACTIVE_TELEMETERS.append(m)
-            if logger is not None and is_logger(meter):
-                logger.addHandler(m)
--- telemetry/load_utils.py
+++ telemetry/load_utils.py
@@ -12,6 +12,23 @@ def is_logger(meter: Telemeter):
     return hasattr(loggers, meter.__name__)
 
 
+def env_loggers():
+    for meter in REGISTRY:
+        if is_configured(meter) and is_logger(meter):
+            yield meter
+
+
+def load_env(logger=None):
+    global ACTIVE_TELEMETERS
+    ACTIVE_TELEMETERS = []  # TODO scan if each telemeter is loaded already.
+    for meter in REGISTRY:
+        if is_configured(meter):
+            m = meter()
+            ACTIVE_TELEMETERS.append(m)
+            if logger is not None and is_logger(meter):
+                logger.addHandler(m)
+
+
 def env_handlers(loggers=False):
     """
     params:
